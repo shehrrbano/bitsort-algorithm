6e9a3b59bdca6c2a7f2d07143d0d92f2f0de491e
Muhammad Ashir Khan <just4fun17678@gmail.com>
2025-06-06 20:52:07 +0500

    First init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gradient-to-br from-amber-100 to-yellow-200">
+      <h1
+        className="text-6xl font-extrabold text-gray-800 transition duration-300 ease-in-out transform hover:scale-110 hover:text-yellow-500 hover:drop-shadow-xl"
+      >
+        Hello Sir!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
